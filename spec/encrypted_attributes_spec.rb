53c89008a3c26f2e269c2270f2e2ab0cb2e43328
Junya Ogura <junyaogura@gmail.com>
2011-02-25 15:17:25 +0900

    add spec for validation.

--- spec/encrypted_attributes_spec.rb
+++ spec/encrypted_attributes_spec.rb
@@ -8,4 +8,11 @@ describe 'Encrypted attributes' do
     subject { credential.password }
     it { should eq('this is not a secret') }
   end
+
+  context "Has invalid attributes" do
+    let(:credential) { Credential.create(:email => nil) }
+    before { credential.password = 'this is not a secret' }
+    subject { credential }
+    it { should_not be_valid }
+  end
 end
